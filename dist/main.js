e96363baa6e95dff05e91394325da9f37a470e52
akorbutovski <akorbutovski@elinext.com>
2023-08-07 17:04:41 +0300

    WP html plugin

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,1 @@
-(() => {
-    document.getElementById("wp_test"), document.querySelectorAll(".image"), Array.from(document.querySelectorAll("audio")), document.querySelectorAll("img");
-    document.getElementById("wp_test").style.backgroundImage = "url(./src/assets/summer-bg.jpg)"
-})();
+(()=>{document.getElementById("wp_test"),document.querySelectorAll(".image"),Array.from(document.querySelectorAll("audio")),document.querySelectorAll("img");document.getElementById("wp_test").style.backgroundImage="url(./src/assets/summer-bg.jpg)"})();
