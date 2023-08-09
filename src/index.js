8bdeeda616213073d9b06f3e7110aa5a0c3dc758
Alex Korbutovski <akorbutovski@elinext.com>
2023-08-09 16:28:33 +0300

    fix: src for img, audio, fonts

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const root = document.getElementById('wp_test');
+
+// function plugJS() {
 const images = document.querySelectorAll('.image')
 const sounds = Array.from(document.querySelectorAll('audio'));
 const pictures = document.querySelectorAll('img');
 const bg = document.getElementById('wp_test');
 let volume = 0.2;
-bg.style.backgroundImage = 'url(./src/assets/summer-bg.jpg)';
+bg.style.backgroundImage = 'url(./summer-bg.jpg)';
 
 const data = [
-    {img: './src/assets/summer-bg.jpg', audio: './src/assets/sounds/summer.mp3', icon: './src/assets/icons/sun.svg'},
     {
-        img: './src/assets/rainy-bg.jpg',
-        audio: './src/assets/sounds/rain.mp3',
-        icon: './src/assets/icons/cloud-rain.svg'
+        img: './public/assets/summer-bg.jpg',
+        audio: './public/assets/sounds/summer.mp3',
+        icon: './src/assets/icons/sun.svg'
     },
     {
-        img: './src/assets/winter-bg.jpg',
-        audio: './src/assets/sounds/winter.mp3',
-        icon: './src/assets/icons/cloud-snow.svg'
+        img: './public/assets/rainy-bg.jpg',
+        audio: './public/assets/sounds/rain.mp3',
+        icon: './public/assets/icons/cloud-rain.svg'
+    },
+    {
+        img: './public/assets/winter-bg.jpg',
+        audio: './public/assets/sounds/winter.mp3',
+        icon: './public/assets/icons/cloud-snow.svg'
     },
 ]
 
@@ -33,17 +39,9 @@ function setBGandPlay() {
             pict.addEventListener('click', getEvent);
         }
     )
-
 }
-// function setBGandPlay() {
-//     pictures.forEach(pict => {
-//             pict.addEventListener('click', getEvent);
-//         }
-//     )
-// }
 
 function getEvent(e) {
-
     images.forEach((image) => {
             if (e.target.alt === image.id) {
                 bg.style.backgroundImage = `url(${data[+image.id].img})`;
@@ -59,21 +57,6 @@ function getEvent(e) {
     )
 }
 
-// function renderImages(item) {
-// function renderImages(item) {
-//     // const bg = document.createElement('img');
-//     // const img = document.createElement('img');
-//     const sound = document.createElement('audio');
-//     const icon = document.createElement('img');
-//
-//     // bg.src = item.src;
-//     image.src = item.img;
-//     block.src = item.img;
-//     sound.src = item.audio;
-//     icon.src = item.icon;
-//     image.append(sound,icon);
-//     block.append(image);
-//     root.append(block);
 // }
-//
-// data.forEach(renderImages);
+// root.append(plugJS())
+// window.onload = plugJS;
